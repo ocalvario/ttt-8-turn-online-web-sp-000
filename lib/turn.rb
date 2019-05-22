28041bb2c3773d20585a99d273036789b8b939f7
Oswald Calvario <oswald.calvario@gmail.com>
2019-05-22 22:39:49 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,8 +10,6 @@ def input_to_index(user_input)
   converted_input = user_input.to_i - 1
 end
 
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
 def valid_move?(board, index)
   if index.between?(1,9)
     if !position_taken?(board, index)
@@ -31,4 +29,18 @@ def move(board,index, position = "X")
   board[new_index]
 end
 
-move(board) = ["O", " ", " ", " ", " ", " ", " ", " ", "X"]
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
